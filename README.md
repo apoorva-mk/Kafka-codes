04fb1541ab05c727ac634b63c3d2769dc0f03002
apoorva-mk <apoorva.mk99@gmail.com>
2020-04-23 11:28:19 +0530

    Producer API

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Kafka-codes
 
 A maven project which contains simple codes for the various Kafka APIs
+
+To run eg: producer
+java -cp target/kafka-code-1.0-SNAPSHOT.jar:/usr/local/kafka/libs/* com.apoorva.kafka.SimpleProducer
+
+
--- src/main/java/com/apoorva/kafka/SimpleProducer.java
+++ src/main/java/com/apoorva/kafka/SimpleProducer.java
@@ -4,21 +4,26 @@ import java.util.*;
 import org.apache.kafka.clients.producer.*;
 
 /**
- * Hello world!
+ * Simple Producer API for single node cluster
+ * Docs: https://kafka.apache.org/25/javadoc/index.html?org/apache/kafka/clients/producer/KafkaProducer.html
  *
  */
-public class App 
+
+
+public class SimpleProducer
 {
     public static void main(String[] args) throws Exception{
            
         Properties props = new Properties();
 		props.put("bootstrap.servers", "localhost:9092");
+
+		//To ensure complete full commit to record, blocking write
 		props.put("acks", "all");
 		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		System.out.println("Configuration Completed.");
 		Producer<String, String> producer = new KafkaProducer<>(props);
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < 10; i++)
 		    producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
 
 		System.out.println("Message sent to topic - test");
